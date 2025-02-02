529ef9d41893a0a4bc21329911c6991b0703c4fe
shahin2525 <shahin451999@gmail.com>
2025-02-01 19:25:15 -0800

    add bike shop

---
+++ src/components/ui/dashboard/Sider.tsx
@@ -0,0 +1,67 @@
+import { Layout, Menu, MenuProps } from "antd";
+import { NavLink } from "react-router-dom";
+
+const { Sider } = Layout;
+const adminItems: MenuProps["items"] = [
+  {
+    key: "create-product",
+    label: <NavLink to="admin/create-product">Create Product</NavLink>,
+  },
+
+  {
+    key: "update-product",
+    label: <NavLink to="admin/update-product">Create Product</NavLink>,
+  },
+  {
+    key: "deactivate-customer",
+    label: <NavLink to="admin/deactivate-customer">Create Product</NavLink>,
+  },
+];
+
+const customerItems: MenuProps["items"] = [
+  {
+    key: "create-order",
+    label: <NavLink to="customer/create-order">Create Product</NavLink>,
+  },
+
+  {
+    key: "update-password",
+    label: <NavLink to="customer/update-password">Change Password</NavLink>,
+  },
+  {
+    key: "order-cancel",
+    label: <NavLink to="order/order-cancel">Order Cancle</NavLink>,
+  },
+];
+
+const Sidebar = () => {
+  const UserRole = {
+    Admin: "admin",
+    Customer: "customer",
+  };
+  return (
+    <Sider>
+      <div
+        style={{
+          color: "white",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          height: "4rem",
+          font: "bold",
+          fontSize: "30px",
+        }}
+      >
+        Bike Shop
+      </div>
+      <Menu
+        theme="dark"
+        mode="inline"
+        defaultSelectedKeys={["4"]}
+        items={adminItems}
+      />
+    </Sider>
+  );
+};
+
+export default Sidebar;
