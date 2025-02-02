f0d37d386e96167c114c6b8476c58aea8f9d8f05
shahin2525 <shahin451999@gmail.com>
2025-02-02 08:58:26 -0800

    add authslice

---
+++ src/redux/feature/auth/authSlice.ts
@@ -0,0 +1,31 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export interface TAuthState {
+  user: null | object;
+  token: string | null;
+}
+
+const initialState: TAuthState = {
+  user: null,
+  token: null,
+};
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setUser: (state, action) => {
+      state.user = action.payload;
+      state.token = action.payload;
+    },
+    logout: (state) => {
+      state.user = null;
+      state.token = null;
+    },
+  },
+});
+
+// Action creators are generated for each case reducer function
+export const { setUser, logout } = authSlice.actions;
+
+export default authSlice.reducer;
