d03d523139ccfbc64f1f3d5fce1b4f5cadd3c451
shahin2525 <shahin451999@gmail.com>
2025-02-11 17:12:59 -0800

    add bike options

--- src/components/ui/user/CreateOrder.tsx
+++ src/components/ui/user/CreateOrder.tsx
@@ -1,8 +1,118 @@
+import { Button, Col, Row } from "antd";
+import PHForm from "../../form/BSForm";
+import PHInput from "../../form/BSInput";
+import BSSelect from "../../form/BSSelect";
+import { toast } from "sonner";
+import { TResponse } from "../../../types/global.types";
+import { FieldValues, SubmitHandler } from "react-hook-form";
+import { useCreateOrderMutation } from "../../../redux/feature/order/orderApi";
+import { useGetAllBikeQuery } from "../../../redux/feature/bike/bikApi";
+
 const CreateOrder = () => {
+  const { data: productData } = useGetAllBikeQuery(undefined);
+  const [createOrder] = useCreateOrderMutation();
+
+  const productOptions = productData?.data?.map((item) => ({
+    value: item._id,
+    label: item.title,
+  }));
+
+  const onsubmit: SubmitHandler<FieldValues> = async (data) => {
+    // console.log("data", Number(data.name) - 1);
+
+    const toastId = toast.loading("creating .....");
+
+    const orderData = {
+      email: data.email,
+
+      product: data?.product,
+
+      quantity: data?.quantity,
+
+      totalPrice: data?.quantity,
+    };
+    try {
+      const res = (await createOrder(orderData)) as TResponse<any>;
+      console.log(res);
+      if (res?.error) {
+        toast.error(res.error.data.message, { id: toastId });
+      } else {
+        toast.success("order create successfully", { id: toastId });
+      }
+    } catch (error: any) {
+      toast.error(error.data.message, { id: toastId });
+    }
+  };
   return (
-    <div>
-      <h1>create order</h1>
-    </div>
+    <Row
+      justify="center"
+      style={{ marginTop: "30px", marginBottom: "20px", padding: "0px 10px" }}
+    >
+      <Col span={24}>
+        <PHForm
+          onSubmit={onsubmit}
+          // resolver={zodResolver(academicSemesterSchema)}
+        >
+          <Row gutter={4}>
+            <Col
+              span={24}
+              md={{ span: 12 }}
+              lg={{ span: 12 }}
+              style={{ padding: "0px 3px" }}
+            >
+              <PHInput label="Email" type="text" name="email" />
+            </Col>
+            <Col
+              span={24}
+              md={{ span: 12 }}
+              lg={{ span: 12 }}
+              style={{ padding: "0px 3px" }}
+            >
+              <BSSelect
+                label="Product"
+                name="product"
+                options={productOptions}
+              />
+            </Col>
+            <Col
+              span={24}
+              md={{ span: 12 }}
+              lg={{ span: 12 }}
+              style={{ padding: "0px 3px" }}
+            >
+              <PHInput label="Quantity" type="text" name="quantity" />
+            </Col>
+          </Row>
+
+          <Row>
+            <Col
+              span={24}
+              md={{ span: 12 }}
+              lg={{ span: 12 }}
+              style={{ padding: "0px 3px" }}
+            >
+              <PHInput label="Total Price" type="text" name="totalPrice" />
+            </Col>
+            <Col
+              span={24}
+              md={{ span: 12 }}
+              lg={{ span: 12 }}
+              style={{ padding: "0px 3px" }}
+            >
+              <PHInput label="Quantity" type="text" name="quantity" />
+            </Col>
+          </Row>
+
+          <Button
+            type="primary"
+            htmlType="submit"
+            style={{ marginLeft: "3px" }}
+          >
+            Submit
+          </Button>
+        </PHForm>
+      </Col>
+    </Row>
   );
 };
 
